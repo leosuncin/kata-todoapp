5e70e010c320013c95a36c299495c6ad1aaad951
Jaime Leonardo Suncin Cruz <leosuncin@users.noreply.github.com>
2024-03-19 21:21:28 +0000

    feat(contract): add task routes to the contract

--- libs/contract/src/contract.ts
+++ libs/contract/src/contract.ts
@@ -1,9 +1,11 @@
 import { initContract } from '@ts-rest/core';
 
 import { authRoutes } from '@contract/auth.js';
+import { taskRoutes } from '@contract/task.js';
 
 const { router } = initContract();
 
 export const contract = router({
   auth: authRoutes,
+  task: taskRoutes,
 });
